ce16b3ea0293402b80af1d10689d1dfa9d24f98d
javoxircoding <144697319+javoxircoding@users.noreply.github.com>
2026-01-08 17:30:43 +0300

    added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+
+function Navbar = () =>  {
+    return (
+        <nav style={{display: "flex",  }}>
+        </nav>
+
+    )
+}
+
+
+// home 
+//about
+//skills
+//projects
+//experince
+//Roadmap
+//contact
