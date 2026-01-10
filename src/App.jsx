817fb8c1dccbecae3443a8c20cd29f69a2a06e73
javoxircoding <144697319+javoxircoding@users.noreply.github.com>
2026-01-10 17:26:52 +0300

    added

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <main style={{textAlign: 'center', }}>
+
+        <h1>Salom Men Javahirman</h1>
+        <p>2 yildan buyun fronted dasturchiman hamda websaytyaratishbo'yicha mutaxassisman </p>
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,42 @@
+function Navbar() {
+  return (
+    <nav
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        padding: "10px 50px",
+        borderBottom: "1 solid  #ddd",
+        alignItems: "center",
+      }}
+    >
+      <div className="logo">
+        <h1>Javoxir</h1>
+      </div>
+      <ul style={{ display: "flex", listStyle: "none", gap: "50px" }}>
+        <li>
+          <a href="#home"></a>Home
+        </li>
+        <li>
+          <a href="about"></a>About
+        </li>
+        <li>
+          <a href="skills"></a>Skills
+        </li>
+        <li>
+          <a href="projects"></a>Project
+        </li>
+        <li>
+          <a href="experians"></a>Experians
+        </li>
+        <li>
+          <a href="Roadmap"></a>Roadmap
+        </li>
+        <li>
+          <a href="contact"></a>Contakt
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-function Navbar = () =>  {
-    return (
-        <nav style={{display: "flex",  }}>
-        </nav>
-
-    )
-}
-
-
-// home 
-//about
-//skills
-//projects
-//experince
-//Roadmap
-//contact
+export default Navbar;
