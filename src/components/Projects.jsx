90aab6d366a88abafa232b6931b7c6ff3d1661c2
javoxircoding <144697319+javoxircoding@users.noreply.github.com>
2026-01-28 22:51:51 +0500

    project qisimlarini qoshdim

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,154 +1,175 @@
-import styles from './Project.module.css';
+import styles from "./Project.module.css";
 
-const Projects = () =>{
-    return(
-        <section>
-            
-                {/* bosh qismi */}
+const Projects = () => {
+  return (
+    <section>
+      {/* bosh qismi */}
 
-            <div className={styles.header}>
-                <span>Loyhalar</span>
-                <h2>Men yaratgan loyhalar</h2>
+      <div className={styles.header}>
+        <span>Loyhalar</span>
+        <h2>Men yaratgan loyhalar</h2>
+      </div>
+
+      <div className={styles.bentoGrid}>
+        <div className={styles.card1}>
+          {/* grid cartalari */}
+
+          <div className={styles.gridCard}>
+            <div className={styles.hover}></div>
+
+            <div className={styles.someInfa}>
+              
+              <h3 className={styles.tarkib}>
+                <span>react</span>
+                <span>js</span>
+                <span>bootstrap</span>
+              </h3>
             </div>
 
-                <div className={styles.bentoGrid}>
-                    <div className={styles.gridCard}>
+            <div className={styles.hoverEfect}>
+              <h1>Nomi</h1>
+              <h3>sayt haqida ozgina malumot</h3>
+              <button>
+                <a href="http://">saytni ko'rish</a>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className={styles.card2}>
+          {/* grid cartalari */}
 
-                            {/* grid cartalari */}
+          <div className={styles.gridCard}>
+            <div className={styles.hover}></div>
+            <div className={styles.someInfa}>
+              
+              <h3 className={styles.tarkib}>
+                <span>react</span>
+                <span>js</span>
+                <span>bootstrap</span>
+              </h3>
+            </div>
+            <div className={styles.hoverEfect}>
+              <h1>Nomi</h1>
+              <h3>sayt haqida ozgina malumot</h3>
+              <button>
+                <a href="http://">saytni ko'rish</a>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className={styles.card3}>
+          {/* grid cartalari */}
 
-                        <div className={styles.card1}>
-                            <img className={styles.bgImege} src="" alt="" />
-                            <h1>Proyect nomi</h1>
-                            <h3 className={styles.tarkib}>
-                                <span>react</span>
-                                <span>js</span>
-                                <span>bootstrap</span>
-                            </h3>
-                        <div className={styles.hoverEfect}>
-                            <h1>Nomi</h1>
-                            <h3>sayt haqida ozgina malumot</h3>
-                            <button>saytni ko'rish</button>
-                        </div>
-                        </div>
-                    </div>
-                    <div className={styles.gridCard}>
+          <div className={styles.gridCard}>
+            <div className={styles.hover}></div>
+            <div className={styles.someInfa}>
+              
+              <h3 className={styles.tarkib}>
+                <span>react</span>
+                <span>js</span>
+                <span>bootstrap</span>
+              </h3>
+            </div>
+            <div className={styles.hoverEfect}>
+              <h1>Nomi</h1>
+              <h3>sayt haqida ozgina malumot</h3>
+              <button>
+                <a href="http://">saytni ko'rish</a>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className={styles.card4}>
+          {/* grid cartalari */}
 
-                            {/* grid cartalari */}
+          <div className={styles.gridCard}>
+            <div className={styles.hover}></div>
+            <div className={styles.someInfa}>
+              
+              <h3 className={styles.tarkib}>
+                <span>react</span>
+                <span>js</span>
+                <span>bootstrap</span>
+              </h3>
+            </div>
+            <div className={styles.hoverEfect}>
+              <h1>Nomi</h1>
+              <h3>sayt haqida ozgina malumot</h3>
+              <button>
+                <a href="http://">saytni ko'rish</a>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className={styles.card5}>
+          {/* grid cartalari */}
 
-                        <div className={styles.card1}>
-                            <img className={styles.bgImege} src="" alt="" />
-                            <h1>Proyect nomi</h1>
-                            <h3 className={styles.tarkib}>
-                                <span>react</span>
-                                <span>js</span>
-                                <span>bootstrap</span>
-                            </h3>
-                        <div className={styles.hoverEfect}>
-                            <h1>Nomi</h1>
-                            <h3>sayt haqida ozgina malumot</h3>
-                            <button>saytni ko'rish</button>
-                        </div>
-                        </div>
-                    </div>
-                    <div className={styles.gridCard}>
+          <div className={styles.gridCard}>
+            <div className={styles.hover}></div>
+            <div className={styles.someInfa}>
+              
+              <h3 className={styles.tarkib}>
+                <span>react</span>
+                <span>js</span>
+                <span>bootstrap</span>
+              </h3>
+            </div>
+            <div className={styles.hoverEfect}>
+              <h1>Nomi</h1>
+              <h3>sayt haqida ozgina malumot</h3>
+              <button>
+                <a href="http://">saytni ko'rish</a>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className={styles.card6}>
+          {/* grid cartalari */}
 
-                            {/* grid cartalari */}
+          <div className={styles.gridCard}>
+            <div className={styles.hover}></div>
+            <div className={styles.someInfa}>
+              
+              <h3 className={styles.tarkib}>
+                <span>react</span>
+                <span>js</span>
+                <span>bootstrap</span>
+              </h3>
+            </div>
+            <div className={styles.hoverEfect}>
+              <h1>Nomi</h1>
+              <h3>sayt haqida ozgina malumot</h3>
+              <button>
+                <a href="http://">saytni ko'rish</a>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className={styles.card7}>
+          {/* grid cartalari */}
 
-                        <div className={styles.card1}>
-                            <img className={styles.bgImege} src="" alt="" />
-                            <h1>Proyect nomi</h1>
-                            <h3 className={styles.tarkib}>
-                                <span>react</span>
-                                <span>js</span>
-                                <span>bootstrap</span>
-                            </h3>
-                        <div className={styles.hoverEfect}>
-                            <h1>Nomi</h1>
-                            <h3>sayt haqida ozgina malumot</h3>
-                            <button>saytni ko'rish</button>
-                        </div>
-                        </div>
-                    </div>
-                    <div className={styles.gridCard}>
-
-                            {/* grid cartalari */}
-
-                        <div className={styles.card1}>
-                            <img className={styles.bgImege} src="" alt="" />
-                            <h1>Proyect nomi</h1>
-                            <h3 className={styles.tarkib}>
-                                <span>react</span>
-                                <span>js</span>
-                                <span>bootstrap</span>
-                            </h3>
-                        <div className={styles.hoverEfect}>
-                            <h1>Nomi</h1>
-                            <h3>sayt haqida ozgina malumot</h3>
-                            <button>saytni ko'rish</button>
-                        </div>
-                        </div>
-                    </div>
-                    <div className={styles.gridCard}>
-
-                            {/* grid cartalari */}
-
-                        <div className={styles.card1}>
-                            <img className={styles.bgImege} src="" alt="" />
-                            <h1>Proyect nomi</h1>
-                            <h3 className={styles.tarkib}>
-                                <span>react</span>
-                                <span>js</span>
-                                <span>bootstrap</span>
-                            </h3>
-                        <div className={styles.hoverEfect}>
-                            <h1>Nomi</h1>
-                            <h3>sayt haqida ozgina malumot</h3>
-                            <button>saytni ko'rish</button>
-                        </div>
-                        </div>
-                    </div>
-                    <div className={styles.gridCard}>
-
-                            {/* grid cartalari */}
-
-                        <div className={styles.card1}>
-                            <img className={styles.bgImege} src="" alt="" />
-                            <h1>Proyect nomi</h1>
-                            <h3 className={styles.tarkib}>
-                                <span>react</span>
-                                <span>js</span>
-                                <span>bootstrap</span>
-                            </h3>
-                        <div className={styles.hoverEfect}>
-                            <h1>Nomi</h1>
-                            <h3>sayt haqida ozgina malumot</h3>
-                            <button>saytni ko'rish</button>
-                        </div>
-                        </div>
-                    </div>
-                    <div className={styles.gridCard}>
-
-                            {/* grid cartalari */}
-
-                        <div className={styles.card1}>
-                            <img className={styles.bgImege} src="" alt="" />
-                            <h1>Proyect nomi</h1>
-                            <h3 className={styles.tarkib}>
-                                <span>react</span>
-                                <span>js</span>
-                                <span>bootstrap</span>
-                            </h3>
-                        <div className={styles.hoverEfect}>
-                            <h1>Nomi</h1>
-                            <h3>sayt haqida ozgina malumot</h3>
-                            <button>saytni ko'rish</button>
-                        </div>
-                        </div>
-                    </div>
-                </div>
-
-        </section>
-    )
-}
+          <div className={styles.gridCard}>
+            <div className={styles.hover}></div>
+            <div className={styles.someInfa}>
+              
+              <h3 className={styles.tarkib}>
+                <span>react</span>
+                <span>js</span>
+                <span>bootstrap</span>
+              </h3>
+            </div>
+            <div className={styles.hoverEfect}>
+              <h1>Nomi</h1>
+              <h3>sayt haqida ozgina malumot</h3>
+              <button>
+                <a href="http://">saytni ko'rish</a>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Projects;
